8c50b5a7cd6aa506d389bef52800ffa2ae857ca1
JosueDanielBust <josuedanielbustamante@gmail.com>
2018-05-12 20:33:05 -0500

    Add semantic base

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(window).stellar({
+    horizontalScrolling: true,
+    verticalScrolling: false,
+    horizontalOffset: 150,
+    verticalOffset: 150,
+    responsive: true,
+    scrollProperty: 'scroll',
+    positionProperty: 'position',
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    hideDistantElements: true,
+});
